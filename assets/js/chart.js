6774ed9db6dc1ec67be4f69fb77bb3b0242380c5
Muhammad Naveed Ashraf <mahernaveed531@gmail.com>
2021-10-09 20:29:35 +0500

    Updated Files with updated charts and edit modals added

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -6,16 +6,17 @@ new Morris.Line({
   // Chart data records -- each entry in this array corresponds to a point on
   // the chart.
   data: [
-    { year: '2008', value: 20 },
-    { year: '2009', value: 10 },
-    { year: '2010', value: 30 },
-    { year: '2011', value: 15 },
-    { year: '2012', value: 30 },
-    { year: '2013', value: 30 },
-    { year: '2014', value: 40 }
+    { week: '1', value: 20 },
+    { week: '2', value: 10 },
+    { week: '3', value: 30 },
+    { week: '4', value: 15 },
+    { week: '5', value: 30 },
+    { week: '6', value: 30 },
+    { week: '7', value: 40 }
   ],
   // The name of the data record attribute that contains x-values.
-  xkey: 'year',
+  xkey: 'week',
+  xLabel: 'week',
   // A list of names of data record attributes that contain y-values.
   ykeys: ['value'],
   // Labels for the ykeys -- will be displayed when you hover over the
@@ -25,13 +26,41 @@ new Morris.Line({
 
                             // Donut Chart
 
+// Morris.Donut({
+//     element: 'progress-donut',
+//     data: [
+//         {label: "Running", value: 12},
+//         {label: "Walking", value: 30},
+//         {label: "Cycling", value: 20},
+//         {label: "Swimming", value: 15}
+//     ],
+//     labelColor: '#fff'
+// })
+
+
+var colorDanger = "#FF1744";
 Morris.Donut({
-    element: 'progress-donut',
-    data: [
-        {label: "Running", value: 12},
-        {label: "Walking", value: 30},
-        {label: "Cycling", value: 20},
-        {label: "Swimming", value: 15}
-    ],
-    labelColor: '#fff'
-})
+  element: 'donut-example',
+  resize: true,
+  colors: [
+    '#E0F7FA',
+    '#B2EBF2',
+    '#80DEEA',
+    '#4DD0E1',
+    '#26C6DA',
+    '#00BCD4',
+    '#00ACC1',
+    '#0097A7',
+    '#00838F',
+    '#006064'
+  ],
+  labelColor:"#fff", // text color
+  backgroundColor: '#333333', // border color
+  data: [
+    {label:"Running", value:10, color:colorDanger},
+    {label:"Walking", value:9},
+    {label:"Cycling", value:15},
+    {label:"Swimming", value:20},
+    {label:"Hicking", value:30}
+  ]
+});
